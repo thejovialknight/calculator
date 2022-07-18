d162bc80863a5ac20bbbf5280cd2a17f39bbc8a9
thejovialknight <thejovialknight@gmail.com>
2022-07-18 14:57:54 -0700

    Implement calculator logic/display sans edge cases

--- script.js
+++ script.js
@@ -1,16 +1,106 @@
 function operate(a, b, operation) {
+	a = Number(a);
+	b = Number(b);
+
 	switch(operation) {
-		case "add":
+		case "+":
 			return a + b;
 			break;
-		case "subtract":
+		case "-":
 			return a - b;
 			break;
-		case "multiply":
+		case "*":
 			return a * b;
 			break;
-		case "divide":
+		case "/":
 			return a / b;
 			break;
 	}
 }
+
+function updateDisplay() {
+	const display = document.querySelector("#display-text");
+	display.innerHTML = "";
+	if(firstOperand !== null) display.innerHTML += firstOperand;
+	if(currentOperator !== null) display.innerHTML += " " + currentOperator;
+	if(secondOperand !== null) display.innerHTML += " " + secondOperand;
+}
+
+function initOperator(documentId, operatorSymbol) {
+	document.getElementById(documentId).addEventListener("click", () => { 
+		if(firstOperand === null) { 
+			return;
+		} 
+
+		if(secondOperand !== null) {
+			resolveCalculation();
+		}
+
+		currentOperator = operatorSymbol;
+		updateDisplay();
+	});
+}
+
+function clearData() {
+	firstOperand = null;
+	secondOperand = null;
+	currentOperator = null;
+}
+
+function clearDisplay() {
+	clearData();
+	updateDisplay();
+}
+
+function resolveCalculation() {
+	if(firstOperand === null || currentOperator === null || secondOperand === null) {
+		return;
+	}
+
+	const results = operate(firstOperand, secondOperand, currentOperator);
+	clearData();
+	firstOperand = results;
+}
+
+function init() {
+	// Setup event listeners
+	initOperator("add", "+");
+	initOperator("subtract", "-");
+	initOperator("multiply", "*");
+	initOperator("divide", "/");
+
+	document.getElementById("clear").addEventListener("click", () => {
+		clearDisplay();
+	});
+
+	document.getElementById("equals").addEventListener("click", () => {
+		resolveCalculation();
+		updateDisplay();
+	});
+
+	for(const numberElement of document.querySelectorAll(".num")) {
+		const number = numberElement.dataset.num; 
+		numberElement.addEventListener("click", () => {
+			if(firstOperand === null) {
+				firstOperand = number;
+			} else {
+				if(currentOperator === null) {
+					firstOperand += number;
+				} else {
+					if(secondOperand === null) {
+						secondOperand = number;
+					} else {
+						secondOperand += number;
+					}
+				}
+			}
+
+			updateDisplay();
+		});
+	}
+}
+
+let firstOperand = null;
+let secondOperand = null;
+let currentOperator = null;
+init();
